3d67855121e1792085165c8fe05338c4d32459d8
Brandon Swann <brandonjswann@gmail.com>
2024-03-13 14:56:57 -0600

    main

---
+++ script.js
@@ -0,0 +1,18 @@
+//pseudocode: create an equation for variable num1 that equals 10
+//pseudocode: create an equation for variable num2 that equals 40
+//pseudocode: create an equation for variable num3 that equals 39
+//pseudocode: Create a variable for a greeting that says, "You have received this message because you have been chosen to open an important vault. Here is the secret combination: num1 - num2 - num3"
+//pseudocode: create an alert that displays the greeting variable with the variables num1, num2 and num3 at the end
+
+// first create the variables with three different equations
+let num1 = 19-18+9
+let num2 = 20*10/5
+let num3 = num2-1
+
+//next create the greeting to include the desired message
+const greeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+//next create the pop-up alert
+//remember to include dashes between variables to make it easier to differentiate the numbers in the pop-up
+//include a space after the greeting and before num1
+alert(greeting + " " + num1 + "-" + num2 + "-" + num3);
